82e40a0550b44f61b3ada46fbaee1b486edc08aa
jie934164066 <13432803741@163.com>
2019-06-14 18:33:41 +0800

    1

--- m_heima_project/src/App.vue
+++ m_heima_project/src/App.vue
@@ -1,23 +1,49 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- header -->
+    <mt-header fixed title="固定在顶部"></mt-header>123
     <router-view/>
+    <nav class="mui-bar mui-bar-tab">
+      <a class="mui-tab-item mui-active" href="#tabbar">
+        <span class="mui-icon mui-icon-home"></span>
+        <span class="mui-tab-label">首页</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-contact">
+        <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">会员</span>
+      </a>
+      <a class="mui-tab-item" href="#tabbar-with-chat">
+        <span class="mui-icon-extra mui-icon-extra-cart">
+          <span class="mui-badge">9</span>
+        </span>
+        <span class="mui-tab-label">购物车</span>
+      </a>
+
+      <a class="mui-tab-item" href="#tabbar-with-map">
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜索</span>
+      </a>
+    </nav>
   </div>
 </template>
 
 <script>
+import { Toast } from "mint-ui";
 export default {
-  name: 'App'
-}
+  name: "App",
+  created() {
+    Toast("提示信息");
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
 }
 </style>
